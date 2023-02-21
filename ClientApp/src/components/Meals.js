66650ca02361a510c13f7c5d117c10bf88cb0335
CBush88 <christopherbush88@gmail.com>
2023-02-21 18:39:22 -0500

    Table view for tracked meals

--- ClientApp/src/components/Meals.js
+++ ClientApp/src/components/Meals.js
@@ -1,4 +1,4 @@
-﻿import React, { useState, useEffect } from 'react';
+﻿import React, { useState } from 'react';
 import { getMeals } from '../services/Meals';
 
 const Meals = () => {
@@ -10,13 +10,48 @@ const Meals = () => {
             })
         .catch(err => console.log(err.response))
     }
-    useEffect(() => {
-        retrieveMeals();
-    },[])
+
+    retrieveMeals();
+
 
     return (
         <>
-            {JSON.stringify(meals)}
+            <table className="table">
+                <tr>
+                    <th>Id</th>
+                    <th>Name</th>
+                    <th>Serving Size</th>
+                    <th>Serving Size Unit</th>
+                    <th>Calories</th>
+                    <th>Carbohydrates</th>
+                    <th>Protein</th>
+                    <th>Fat</th>
+                    <th>Phosphorus</th>
+                    <th>Potassium</th>
+                    <th>Sodium</th>
+                    <th>Servings</th>
+                    <th>Date</th>
+                </tr>
+                {meals.map(meal => {
+                    return (
+                        <tr>
+                            <td>{meal.mealId}</td>
+                            <td>{meal.foodName}</td>
+                            <td>{meal.servingSize}</td>
+                            <td>{meal.servingSizeUnit}</td>
+                            <td>{meal.caloriesPerServing}</td>
+                            <td>{meal.carbohydratesPerServing}</td>
+                            <td>{meal.proteinPerServing}</td>
+                            <td>{meal.fatPerServing}</td>
+                            <td>{meal.phosphorusPerServing}</td>
+                            <td>{meal.potassiumPerServing}</td>
+                            <td>{meal.sodiumPerServing}</td>
+                            <td>{meal.servings}</td>
+                            <td>{meal.date}</td>
+                        </tr>
+                    )
+                })}
+            </table>
         </>
         )
 
