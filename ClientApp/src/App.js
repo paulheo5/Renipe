2fe27db245fbeea2ed1703d8fb674e5a2f4f5238
Paul Heo <pheo@macalester.edu>
2023-02-21 14:36:14 -0600

    Working on table schema

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -3,20 +3,64 @@ import { Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
 import { Layout } from './components/Layout';
 import './custom.css';
+import { useEffect } from 'react';
+import UserData from './componenets/UserData.js';
 
-export default class App extends Component {
-  static displayName = App.name;
 
-  render() {
-    return (
-      <Layout>
-        <Routes>
-          {AppRoutes.map((route, index) => {
-            const { element, ...rest } = route;
-            return <Route key={index} {...rest} element={element} />;
-          })}
-        </Routes>
-      </Layout>
-    );
-  }
+const API = "https://api.spoonacular.com/recipes/661447/information?apiKey=5828cde1106e400b9469ae1a2f9732ee"
+
+const App = () => {
+    const [users, setUsers] = useState([]);
+    const fetchUsers = async (url) => {
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            if (data.length > 0) {
+
+                setUsers(data);
+            }
+            console.log(data);
+        } catch (e) {
+            console.error(e)
+        }
+    }
+    useEffect(() => {
+        fetchUsers(API);
+    }, [])
+    return <>
+        <table>
+            <thead>
+                <th>ID</th>
+                <th>Name</th>
+                <th>SourceURL</th>
+            </thead>
+            <tbody>
+                <UserData users={users}/>
+            </tbody>
+        </table>
+    </>
 }
+
+export default App;
+
+
+
+
+//export default class App extends Component {
+//  static displayName = App.name;
+
+//  render() {
+//      return (
+//          <><Layout>
+//              <h1>Table</h1>
+//              <Routes>
+                  
+//                  {AppRoutes.map((route, index) => {
+//                      const { element, ...rest } = route;
+//                      return <Route key={index} {...rest} element={element} />;
+//                  })}
+//              </Routes>
+//          </Layout></>
+//    );
+//  }
+//}
