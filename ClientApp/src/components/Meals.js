9eef858be71b965ee119ed2406764d278f8a1e07
CBush88 <christopherbush88@gmail.com>
2023-02-21 18:08:35 -0500

    Created Meals component to test connection with backend

---
+++ ClientApp/src/components/Meals.js
@@ -0,0 +1,24 @@
+﻿import React, { useState, useEffect } from 'react';
+import { getMeals } from '../services/Meals';
+
+const Meals = () => {
+    const [meals, setMeals] = useState([]);
+    const retrieveMeals = () => {
+        getMeals()
+            .then(res => {
+                setMeals(res.data)
+            })
+        .catch(err => console.log(err.response))
+    }
+    useEffect(() => {
+        retrieveMeals();
+    },[])
+
+    return (
+        <>
+            {JSON.stringify(meals)}
+        </>
+        )
+
+}
+export default Meals
