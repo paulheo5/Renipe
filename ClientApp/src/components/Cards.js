de5e237bc7c4932495e8bc03eb4040c8a0ada3e8
Paul Heo <pheo@macalester.edu>
2023-02-21 21:05:26 -0600

    Modifying home view with components

---
+++ ClientApp/src/components/Cards.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './components/CardItem';
+//import UserData from './components/UserData';
+function Cards() {
+  return (
+    <div>
+     HELLO!
+    </div>
+  );
+}
+
+export default Cards;
