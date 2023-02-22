43caa4d8a338f6333407091a30e0a9eafa96c633
CBush88 <christopherbush88@gmail.com>
2023-02-22 17:40:03 -0500

    Food to NutritionFacts conversion and table implemented

--- ClientApp/src/components/AddMeal.js
+++ ClientApp/src/components/AddMeal.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
 import { searchFdc } from '../services/Fdc'
-import {Button} from './Button'
 
 const AddMeal = () => {
 
@@ -17,9 +16,46 @@ const AddMeal = () => {
         e.preventDefault()
         searchFdc(searchTerm)
         .then(res => {
-            setResults(res.data)
+            setResults(res.data.foods.map(food =>{
+                return foodToNutrition(food)
+            }))
         })
         .catch(err => console.log(err.response))
+    }
+
+    const foodToNutrition = (food) =>{
+        const calories = 1008
+        const protein = 1003
+        const fat = 1004
+        const carbs = 1005
+        const phosphorus = 1091
+        const potassium = 1092
+        const sodium = 1093
+
+        const FoodName = food.description;
+        let CaloriesPerServing = (food.foodNutrients.filter(nutrient => nutrient.nutrientId === calories)[0]?.value) ?? -1
+        const CarbohydratesPerServing = (food.foodNutrients.filter(nutrient => nutrient.nutrientId === carbs)[0]?.value) ?? 0
+        const ProteinPerServing = (food.foodNutrients.filter(nutrient => nutrient.nutrientId === protein)[0]?.value) ?? 0
+        const FatPerServing = (food.foodNutrients.filter(nutrient => nutrient.nutrientId === fat)[0]?.value) ?? 0
+        const PhosphorusPerServing = (food.foodNutrients.filter(nutrient => nutrient.nutrientId === phosphorus)[0]?.value) ?? 0
+        const PotassiumPerServing = (food.foodNutrients.filter(nutrient => nutrient.nutrientId === potassium)[0]?.value) ?? 0
+        const SodiumPerServing = (food.foodNutrients.filter(nutrient => nutrient.nutrientId === sodium)[0]?.value) ?? 0
+
+        if(CaloriesPerServing === -1){
+            CaloriesPerServing = Math.round((4 * CarbohydratesPerServing + ProteinPerServing) + (9 * FatPerServing))
+        }
+
+
+        return({
+            "FoodName" : FoodName,
+            "CaloriesPerServing" : CaloriesPerServing,
+            "CarbohydratesPerServing": CarbohydratesPerServing,
+            "ProteinPerServing" : ProteinPerServing,
+            "FatPerServing" : FatPerServing,
+            "PhosphorusPerServing" : PhosphorusPerServing,
+            "PotassiumPerServing" : PotassiumPerServing,
+            "SodiumPerServing" : SodiumPerServing
+        })
     }
 
   return (
@@ -28,10 +64,37 @@ const AddMeal = () => {
             <input id='search' type='search' placeholder='Meal Search' onChange={handleSearch}></input>
             <button type='submit'>Search</button>
         </form>
-        <Button />
-        <button className="btn-success">Test</button>
+        {/* {JSON.stringify(results)} */}
         <br />
-        {JSON.stringify(results)}
+        <br />
+        <table className='table'>
+            <tr className='bg-dark'>
+            <th className='text-light'>Name</th>
+            <th className='text-light'>Calories</th>
+            <th className='text-light'>Carbohydrates</th>
+            <th className='text-light'>Protein</th>
+            <th className='text-light'>Fat</th>
+            <th className='text-light'>Phosphorus</th>
+            <th className='text-light'>Potassium</th>
+            <th className='text-light'>Sodium</th>
+            </tr>
+        {results.map( food =>{
+            return(
+                <tr>
+                    <td>{food.FoodName}</td>
+                    <td>{food.CaloriesPerServing}</td>
+                    <td>{food.CarbohydratesPerServing}</td>
+                    <td>{food.ProteinPerServing}</td>
+                    <td>{food.FatPerServing}</td>
+                    <td>{food.PhosphorusPerServing}</td>
+                    <td>{food.PotassiumPerServing}</td>
+                    <td>{food.SodiumPerServing}</td>
+                    </tr>
+                
+            )
+        })}
+        </table>
+
     </div>
   )
 
