6053e8ce70f4a48330fc832f599a7421eaeede32
CBush88 <christopherbush88@gmail.com>
2023-02-23 18:06:36 -0500

    Used NutritionFacts Component and sorted by date

--- ClientApp/src/components/Meals.js
+++ ClientApp/src/components/Meals.js
@@ -1,42 +1,54 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getMeals } from '../services/Meals';
 import {Link} from 'react-router-dom';
+import NutritionFacts from './NutritionFacts';
 
 const Meals = () => {
     const [meals, setMeals] = useState([]);
+    
     const retrieveMeals = () => {
         getMeals()
             .then(res => {
-                setMeals(res.data)
+                setMeals(res.data.sort((a, b) => {
+                    if(a.date > b.date){
+                        return -1
+                    }
+                }))
             })
         .catch(err => console.log(err.response))
     }
 
-    
+    useEffect(() => {
+      retrieveMeals()
+    },[])
 
-    retrieveMeals();
+    const style = {"padding":"3px", "paddingLeft":"10px", "paddingRight":"10px"}
 
     return (
         <>
             <table className="table">
+                <thead>
                 <tr>
-                    <th>Id</th>
-                    <th>Name</th>
-                    <th>Serving Size</th>
-                    <th>Serving Size Unit</th>
-                    <th>Calories</th>
-                    <th>Carbohydrates</th>
-                    <th>Protein</th>
-                    <th>Fat</th>
-                    <th>Phosphorus</th>
-                    <th>Potassium</th>
-                    <th>Sodium</th>
-                    <th>Servings</th>
-                    <th>Date</th>
+                    {/* <th style={style}>Id</th> */}
+                    <th style={style}>Name</th>
+                    {/* <th style={style}>Serving Size</th>
+                    <th style={style}>Serving Size Unit</th> */}
+                    <th style={style}>Calories</th>
+                    <th style={style}>Carbohydrates</th>
+                    <th style={style}>Protein</th>
+                    <th style={style}>Fat</th>
+                    <th style={style}>Phosphorus</th>
+                    <th style={style}>Potassium</th>
+                    <th style={style}>Sodium</th>
+                    <th style={style}>Servings</th>
+                    <th style={style}>Date</th>
+                    <th style={style}></th>
                 </tr>
+                </thead>
                 {meals.map(meal => {
                     return (
-                        <tr>
+                        <React.Fragment key={meal.mealId}>
+                        {/* <tr key={meal.mealId}>
                             <td>{meal.mealId}</td>
                             <td>{meal.foodName}</td>
                             <td>{meal.servingSize}</td>
@@ -50,7 +62,13 @@ const Meals = () => {
                             <td>{meal.sodiumPerServing}</td>
                             <td>{meal.servings}</td>
                             <td>{meal.date}</td>
-                        </tr>
+                            <td>
+                                <button className="bg-danger text-light">Delete</button>
+                                <button style={{"marginLeft":"1em"}} className="bg-warning text-dark">Edit</button>
+                            </td>
+                        </tr> */}
+                        <NutritionFacts food={meal} mealView={true} style={style} meals={meals} setMeals={setMeals} />
+                        </React.Fragment>
                     )
                 })}
             </table>
