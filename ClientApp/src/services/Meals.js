8908f1bcf32436e26a892208a9039dfbfae7d440
CBush88 <christopherbush88@gmail.com>
2023-02-23 18:07:17 -0500

    Added update and delete functions

--- ClientApp/src/services/Meals.js
+++ ClientApp/src/services/Meals.js
@@ -5,3 +5,5 @@ const baseurl = `https://localhost:7239/api/meals`
 export const getMeals = () => axios.get(baseurl);
 export const getMealById = (id) => axios.get(baseurl + `${id}`)
 export const trackMeal = (meal) => axios.post(baseurl, meal)
+export const updateMeal = (id, meal) => axios.put(baseurl+`/${id}`, meal)
+export const deleteMeal = (id) => axios.delete(baseurl + `/${id}`)
