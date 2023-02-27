84161b1e4e5347f5c7f5dca4d237144ede16f187
CBush88 <christopherbush88@gmail.com>
2023-02-27 14:30:18 -0500

    Changed API Key after hitting limit

--- ClientApp/src/services/Spoonacular.js
+++ ClientApp/src/services/Spoonacular.js
@@ -1,7 +1,7 @@
 ﻿import axios from "axios";
 
- const apiKey = '5828cde1106e400b9469ae1a2f9732ee'
-// const apiKey = '36d0332aea3241798e916aa4cbd2a928'
+//  const apiKey = '5828cde1106e400b9469ae1a2f9732ee'
+const apiKey = '36d0332aea3241798e916aa4cbd2a928'
 //const apiKey = '5eb6d8fc58b34054a0122c0d074744fe'
 
 export const searchSpoon = (searchTerm) => {
