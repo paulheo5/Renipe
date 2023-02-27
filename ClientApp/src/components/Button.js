e8898cd6eb370a4ea9aef3b34346e17a224eb7cc
Paul Heo <pheo@macalester.edu>
2023-02-27 11:34:45 -0600

    Customizing buttons in recipe info

--- ClientApp/src/components/Button.js
+++ ClientApp/src/components/Button.js
@@ -14,7 +14,7 @@ import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
 
-const STYLES = ['btn--primary', 'btn--outline', 'btn--test'];
+const STYLES = ['btn--primary', 'btn--outline', 'btn--test', 'btn--outline2'];
 
 const SIZES = ['btn--medium', 'btn--large'];
 
