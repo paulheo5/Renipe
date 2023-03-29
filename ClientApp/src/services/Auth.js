1c63b87a62232d06ad306f0f0104a9f05888d4e7
CBush88 <christopherbush88@gmail.com>
2023-03-29 15:13:23 -0400

    Added logout function

--- ClientApp/src/services/Auth.js
+++ ClientApp/src/services/Auth.js
@@ -6,3 +6,6 @@ export const register = (user) => {
 export const login = (user) => {
     return axios.post("https://localhost:7239/api/Auth/login", user);
 }
+export const logout = () => {
+    localStorage.removeItem("token");
+}
