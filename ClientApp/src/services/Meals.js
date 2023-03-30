842b40b2f5317411d60dffcfe21606d47c09c2c5
CBush88 <christopherbush88@gmail.com>
2023-03-30 12:08:52 -0400

    Added token to axios methods

--- ClientApp/src/services/Meals.js
+++ ClientApp/src/services/Meals.js
@@ -2,12 +2,28 @@
 
 const baseurl = `https://localhost:7239/api/meals`
 
-export const getMeals = () => axios.get(baseurl);
-export const getMealById = (id) => axios.get(baseurl + `${id}`)
-export const trackMeal = (meal) => axios.post(baseurl, meal)
+export const getMeals = (token) => axios.get(baseurl, {
+    headers: {
+        Authorization: token
+    }
+});
+export const getMealById = (id, token) => axios.get(baseurl + `${id}`, {
+    headers: {
+        Authorization: token
+    }
+});
+export const trackMeal = (meal, token) => axios.post(baseurl, meal, {
+    headers: {
+        Authorization: token
+    }
+});
 export const updateMeal = (id, meal, token) => axios.put(baseurl+`/${id}`, meal, {
     headers: {
         Authorization: token
     }
-})
-export const deleteMeal = (id) => axios.delete(baseurl + `/${id}`)
+});
+export const deleteMeal = (id, token) => axios.delete(baseurl + `/${id}`, {
+    headers: {
+        Authorization: token
+    }
+});
