caa4bfb4113ce44632648594df4c923a8e8363b3
CBush88 <christopherbush88@gmail.com>
2023-03-30 12:43:15 -0400

    Added UserId to Meal objects
    Requires Truncate and Update-Database

--- Migrations/20230330161119_add_userId_to_meals.Designer.cs
+++ Migrations/20230330161119_add_userId_to_meals.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Renipe.DataContext;
 
@@ -11,9 +12,11 @@ using Renipe.DataContext;
 namespace Renipe.Migrations
 {
     [DbContext(typeof(RenipeDBContext))]
-    partial class RenipeDBContextModelSnapshot : ModelSnapshot
+    [Migration("20230330161119_add_userId_to_meals")]
+    partial class add_userId_to_meals
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
