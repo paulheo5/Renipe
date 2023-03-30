98c59c67d2d8fe662e637a9b0ce76c88e3e02479
CBush88 <christopherbush88@gmail.com>
2023-03-30 10:20:30 -0400

    Add token to put requests in headers

--- ClientApp/src/services/Meals.js
+++ ClientApp/src/services/Meals.js
@@ -5,5 +5,9 @@ const baseurl = `https://localhost:7239/api/meals`
 export const getMeals = () => axios.get(baseurl);
 export const getMealById = (id) => axios.get(baseurl + `${id}`)
 export const trackMeal = (meal) => axios.post(baseurl, meal)
-export const updateMeal = (id, meal) => axios.put(baseurl+`/${id}`, meal)
+export const updateMeal = (id, meal, token) => axios.put(baseurl+`/${id}`, meal, {
+    headers: {
+        Authorization: token
+    }
+})
 export const deleteMeal = (id) => axios.delete(baseurl + `/${id}`)
