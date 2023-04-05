f050ccb8e13eb43975e3ccaea19dbc2f033a5d47
CBush88 <christopherbush88@gmail.com>
2023-04-05 15:41:38 -0400

    Added user, userId to recipes ,added Migration, updated database

---
+++ Migrations/20230405193102_add_userId_to_recipes.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Renipe.Migrations
+{
+    /// <inheritdoc />
+    public partial class add_userId_to_recipes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "user_id",
+                table: "saved_recipe",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_saved_recipe_user_id",
+                table: "saved_recipe",
+                column: "user_id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_saved_recipe_Users_user_id",
+                table: "saved_recipe",
+                column: "user_id",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_saved_recipe_Users_user_id",
+                table: "saved_recipe");
+
+            migrationBuilder.DropIndex(
+                name: "IX_saved_recipe_user_id",
+                table: "saved_recipe");
+
+            migrationBuilder.DropColumn(
+                name: "user_id",
+                table: "saved_recipe");
+        }
+    }
+}
